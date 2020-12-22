546ccd876eab145d64ae1b2e36103a4d1c895908
Jeongbong Seo <jngbng@gmail.com>
2020-12-23 01:38:52 +0900

    unused-sub-key 찾는 기능 추가

--- src/re_frame_lint/core.clj
+++ src/re_frame_lint/core.clj
@@ -189,11 +189,26 @@
                                             file))))))
 
 (defn- find-unknown-sub-key [call-info]
-  (let [registered-sub-key (->> (:decl-sub-key call-info)
-                                (map :key)
-                                (set))]
+  (let [registered-sub-keys (->> (:decl-sub-key call-info)
+                                 (map :key)
+                                 (set))]
     (->> (:refer-sub-key call-info)
-         (filter (comp not registered-sub-key :key)))))
+         (filter (comp not registered-sub-keys :key)))))
+
+(defn- print-info [info]
+  (println (:filepath info) "line:" (:line info) "column:" (:column info) (:key info)))
+
+(defn- print-infos [infos]
+  (doseq [info infos]
+    (print-info info))
+  (println ""))
+
+(defn- lint-unknown-sub-keys [call-info]
+  (let [unknown-keys (find-unknown-sub-key call-info)]
+    (when (seq unknown-keys)
+      (prn "Unknown sub keys:")
+      (print-infos unknown-keys)
+      true)))
 
 (defn- find-unknown-event-key [call-info]
   (let [registered-event-key (->> (:decl-event-key call-info)
@@ -202,24 +217,52 @@
     (->> (:refer-event-key call-info)
          (filter (comp not registered-event-key :key)))))
 
-(defn- lint-unknown-sub-keys [call-info]
-  (let [unknown-keys (find-unknown-sub-key call-info)]
-    (when (seq unknown-keys)
-      (prn "Unknown sub keys:" unknown-keys)
-      true)))
-
 (defn- lint-unknown-event-keys [call-info]
   (let [unknown-keys (find-unknown-event-key call-info)]
       (when (seq unknown-keys)
-        (prn "Unknown event keys:" unknown-keys)
+        (prn "Unknown event keys:")
+        (print-infos unknown-keys)
         true)))
+
+(defn- find-unused-sub-key [call-info]
+  (let [used-keys (->> (:refer-sub-key call-info)
+                          (map :key)
+                          (set))]
+    (->> (:decl-sub-key call-info)
+         (filter (comp not used-keys :key)))))
+
+(defn- lint-unused-sub-keys [call-info]
+  (let [unused-keys (find-unused-sub-key call-info)]
+    (when (seq unused-keys)
+      (prn "Unusued sub keys:")
+      (print-infos unused-keys)
+      true)))
+
+
+(defn- find-unused-event-key [call-info]
+  (let [used-keys (->> (:refer-event-key call-info)
+                       (map :key)
+                       (set))]
+    (->> (:decl-event-key call-info)
+         (filter (comp not used-keys :key)))))
+
+(defn- lint-unused-event-keys [call-info]
+  (let [unused-keys (find-unused-event-key call-info)]
+    (when (seq unused-keys)
+      (prn "Unused event keys:")
+      (print-infos unused-keys)
+      true)))
 
 (defn- lint [opts]
   (let [call-info (collect-call-info opts)
         has-unknown-sub-key? (lint-unknown-sub-keys call-info)
-        has-unknown-event-key? (lint-unknown-event-keys call-info)]
+        has-unknown-event-key? (lint-unknown-event-keys call-info)
+        has-unused-sub-key? (lint-unused-sub-keys call-info)
+        #_#_has-unused-event-key? (lint-unused-event-keys call-info)]
     {:some-warnings (or has-unknown-sub-key?
-                        has-unknown-event-key?)}))
+                        has-unknown-event-key?
+                        has-unused-sub-key?
+                        #_has-unused-event-key?)}))
 
 (defn- lint-from-cmdline [opts]
   (let [ret (lint opts)]
