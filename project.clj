229758b61f0c889f5bc2b3a728deda564b32c9f4
Jeongbong Seo <jngbng@gmail.com>
2021-06-22 10:04:18 +0900

    clojure 버전 업

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/bgpworks/re-frame-lint"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/clojurescript "1.10.764"]]
   :main ^:skip-aot re-frame-lint.core
   :target-path "target/%s"
